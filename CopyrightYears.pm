2931b7262acfefe91aadc2ba9301b6d782a635c0
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2025-01-21 18:27:15 +0100

    Prepare for 0.06 version

--- CopyrightYears.pm
+++ CopyrightYears.pm
@@ -12,7 +12,7 @@ use Pod::CopyrightYears;
 use Perl6::Slurp qw(slurp);
 use String::UpdateYears qw(update_years);
 
-our $VERSION = 0.05;
+our $VERSION = 0.06;
 
 # Constructor.
 sub new {
@@ -238,6 +238,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.05
+0.06
 
 =cut
--- t/App-Perl-Module-CopyrightYears/02-version.t
+++ t/App-Perl-Module-CopyrightYears/02-version.t
@@ -6,4 +6,4 @@ use Test::More 'tests' => 2;
 use Test::NoWarnings;
 
 # Test.
-is($App::Perl::Module::CopyrightYears::VERSION, 0.05, 'Version.');
+is($App::Perl::Module::CopyrightYears::VERSION, 0.06, 'Version.');
